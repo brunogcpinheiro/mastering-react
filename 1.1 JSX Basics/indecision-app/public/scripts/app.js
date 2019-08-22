5a4b1ff1e585328ef8790ba92f2db8434a431529
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2019-08-22 18:18:52 +0000

    finish v1.0 Indecision app

--- 1.1 JSX Basics/indecision-app/public/scripts/app.js
+++ 1.1 JSX Basics/indecision-app/public/scripts/app.js
@@ -2,21 +2,96 @@
 
 console.log("App is running...");
 
-var template = React.createElement(
+var app = {
+  name: "Bruno",
+  options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+  var randomNumber = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNumber];
+  alert(option);
+};
+
+var render = function render() {
+  var template = React.createElement(
     "div",
     null,
     React.createElement(
-        "h1",
-        null,
-        "Hello, World!"
+      "h1",
+      null,
+      "Indecision App"
     ),
     React.createElement(
-        "p",
+      "p",
+      null,
+      app.name,
+      "! Put your life in the hands of computer!"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      "You have ",
+      app.options.length,
+      " things to do!"
+    ),
+    React.createElement(
+      "button",
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      "What should I do?"
+    ),
+    React.createElement(
+      "button",
+      { onClick: onRemoveAll },
+      "Remove all"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
         null,
-        "This is pretty awesome!"
+        "Add Option"
+      )
     )
-);
+  );
+
+  ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.getElementById("root");
 
-ReactDOM.render(template, appRoot);
+render();
--- 1.1 JSX Basics/indecision-app/src/app.js
+++ 1.1 JSX Basics/indecision-app/src/app.js
@@ -1,12 +1,56 @@
 console.log("App is running...");
 
-const template = (
+const app = {
+  name: "Bruno",
+  options: []
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNumber = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNumber];
+  alert(option);
+};
+
+const render = () => {
+  const template = (
     <div>
-        <h1>Hello, World!</h1>
-        <p>This is pretty awesome!</p>
+      <h1>Indecision App</h1>
+      <p>{app.name}! Put your life in the hands of computer!</p>
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <p>You have {app.options.length} things to do!</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemoveAll}>Remove all</button>
+      <ol>
+          {app.options.map(option => {
+              return <li key={option}>{option}</li>
+          })}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
-);
+  );
+
+  ReactDOM.render(template, appRoot);
+};
 
 const appRoot = document.getElementById("root");
 
-ReactDOM.render(template, appRoot);
+render();
